79bb3313ce17feb68ac77638abe6d4e7ad0bc97e
luka177 <lukapanio@gmail.com>
2022-11-06 10:55:24 +0100

    arm64: dts: mediatek: mt6768: add eMMC support

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -7,6 +7,7 @@
  *  Copyright (C) 2005-2008 Pierre Ossman, All Rights Reserved.
  *  MMCv4 support Copyright (C) 2006 Philip Langdale, All Rights Reserved.
  */
+#define DEBUG
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/interrupt.h>
